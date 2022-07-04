cdacf997512ca3e43c8514d8b33276e8e33a0d77
nowmac12 <59921876+nowmac12@users.noreply.github.com>
2022-07-04 16:06:28 +0900

    Add Publisher

--- kf.py
+++ kf.py
@@ -24,13 +24,14 @@ import rospy
 from std_msgs.msg import Float64
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose2D
 from control_to_serial.msg import fromERP
 
 # ERP-42 Parameters
 max_steer = np.radians(28.0)  # [rad] max steering angle
 L = 1.04                      # [m] Wheel base of vehicle
 Lr = L / 2.0                  # [m]
-Lf = L - Lr
+Lf = L - Lr                   # [m]  
 LENGTH = 2.02                 # [m]
 WIDTH = 1.16                  # [m]
 
@@ -38,7 +39,7 @@ Cf = 1600.0 * 2.0  # [N/rad]
 Cr = 1700.0 * 2.0  # [N/rad]
   
 m = 400.0  # [kg]
-Iz = 1500# [kg/m2]    #m*(LENGTH**2 + WIDTH**2)/12
+Iz = 1500  # [kg/m2]    # Moment of Inertia :  m*(LENGTH**2 + WIDTH**2)/12
 
 class linearKalmanFilter():
     
@@ -46,22 +47,20 @@ class linearKalmanFilter():
         self.isFirstTime = True
         self.prevSensorData = np.zeros((4,1))
 
-        # ERP Parameter
+        # <Bicycle Model Parameter>
         self.dt = 0.01667 # [sec]
         self.c_r1 = 0.1
         self.c_a = 0.001
 
-        # Kalman Filter Parameter
+        # <Kalman Filter Parameter> 
+        # -- Initialize X_0, P_0
         self.X = np.array([x, y, yaw, vx, vy, omega]).reshape(6,1)
         self.P = np.diag([0.1, 0.1, np.deg2rad(3), 0.01, 0.01, np.deg2rad(0.01)]) ** 2 
-        # -- Hyper Parameter of External Disturbance
+        # -- Hyper Parameter for External Disturbance
         self.Q = np.diag([0.2, 0.2, np.deg2rad(8), 0.1, 0.1, np.deg2rad(0.7)]) ** 2
-        # -- Hyper Parameter of Sensor Uncertainty
+        # -- Hyper Parameter for Sensor Uncertainty
         self.R = np.diag([0.08, 0.08, np.deg2rad(10), np.deg2rad(1)]) ** 2
-        # self.gpsXs = []
-        # self.gpsYs = []
-        # self.gpsYaws = []
-        # self.kfStates = self.X.copy()
+
         paramList = ('gpsX',
                      'gpsY',
                      'gpsHeading',
@@ -73,27 +72,38 @@ class linearKalmanFilter():
         for paramName in paramList:
             exec('self.'+ paramName + '= 0')
 
+        # Declare Subscribers
         self.gpsOdomSub = rospy.Subscriber('/odom',Odometry, self.gpsOdomCallback, queue_size=1)
         self.gpsHeadingSub = rospy.Subscriber('/current_yaw',Float64, self.gpsHeadingCallback, queue_size=1)
         self.ERPSub = rospy.Subscriber('/erpData',fromERP, self.ERPCallback, queue_size=1)
         self.imuSub = rospy.Subscriber('/imu', Imu, self.imuCallback)
         
-        
+        # Declare Publisher
+        self.posePub = rospy.Publisher('/kalmanFilteredData',Pose2D)
+    
+    # <GPS X, Y callback function>
     def gpsOdomCallback(self, odom):
         self.gpsX = odom.pose.pose.position.x   # utm x [m]
         self.gpsY = odom.pose.pose.position.y   # utm y [m]
-
-        # self.gpsXs.append(self.gpsX)
-        # self.gpsYs.append(self.gpsY)
-        
+    
+    # <GPS Heading callback function>
+    # You need to execute command below
+    # "rosrun nmea_vtg_reader heading_using_vector"
     def gpsHeadingCallback(self, heading): 
         self.gpsHeading = heading.data          # [rad]
-        # self.gpsYaws.append(self.gpsHeading)
 
+    # <IMU sensor callback function>
+    # I only use the 'Yawrate' data of IMU Sensor
+    # and this function returns 'calcState()' function. Because we need the fastest update speed
+    # GPS => 8Hz, ERP => 16Hz, IMU => 30~50Hz 
     def imuCallback(self, imu):
         self.imuYawRate = imu.angular_velocity.z * np.pi/180  # [rad/s]
         return self.calcState()
     
+    # <ERP Data callback function>
+    # You can get 'current front steering wheel angle' and 'speed of the vehicle'
+    # For the Bicycle Model, we have to calculate 'acceleration' of the vehicle [erpThrottle]
+    # Ref) the last page of the ERP-42 Platform Manual doc  
     def ERPCallback(self, ERP):
         self.erpSteer = ERP.steer / 71 * np.pi/180 # [rad]
         self.erpThrottle = -(self.erpSpeed - ERP.speed / 36) / self.dt # [m/s^2]
@@ -192,12 +202,19 @@ class linearKalmanFilter():
         # 3.----Estimate----
         # Estimate The State with Sensors
         self.X = self.X + K @ C @ (Z - H @ self.X)   #X = X + K @ (Z - H @ X)
-        
         # Estimate The Covariance of State
         self.P = P - K @ C @ H @ P #P = P - K @ H @ P
         
+        # Store the Previous Sensor Data for check sensor updated
         self.prevSensorData = Z
-        # self.kfStates = np.hstack((self.kfStates, self.X))
-        # print(f"X : {np.float(self.X[0])}")
-        print(f"Yaw : {np.float(self.X[2]) * 180 / np.pi}")
+
+        # Publish Estimated Data
+        pubData = Pose2D()
+        pubData.x = np.float(self.X[0])
+        pubData.y = np.float(self.X[1])
+        pubData.theta = np.float(self.X[2])
+        self.posePub.publish(pubData)
+
+        # Show 'Yaw' Data
+        rospy.loginfo(f"Yaw : {np.float(self.X[2]) * 180 / np.pi}")
 
