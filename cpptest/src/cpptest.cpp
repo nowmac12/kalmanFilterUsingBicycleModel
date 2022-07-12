9a69473179b7deb001116067d087fad2c4e8bffe
nowmac12 <59921876+nowmac12@users.noreply.github.com>
2022-07-12 18:23:22 +0900

    have to modify params

--- cpptest/src/cpptest.cpp
+++ cpptest/src/cpptest.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <unistd.h>
 #include <cmath>
+#include <numeric>
 #include <vector>
 #include <stdlib.h>
 #include <typeinfo>
+#include <string>
 
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 #include <std_msgs/Float64.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Pose2D.h>
 #include <control_to_serial/fromERP.h>
 
 #include <eigen3/Eigen/Dense>
 
+#define PASS (void)0
 
 // ERP-42 Platform Parameters
 #define MAX_STEER  28.0     // [deg]
 #define L  1.04             // [m]
 #define LR 0.52             // [m]
 #define LF 0.52             // [m]
-#define CR  1700.0 * 2.0    
-#define CF  1600.0 * 2.0
-#define M  400.0            // [kg]
-#define IZ 1500             // [kg/m^2]
+#define CR  1700 * 2.0    
+#define CF  1700 * 2.0
+#define M  225            // [kg]
+#define IZ 219             // [kg/m^2]
 
 using namespace std;
 using namespace Eigen;
@@ -38,6 +46,12 @@ struct vehicleState{
 
 double deg2rad(double deg){
     return deg * M_PI / 180;
+}
+
+double normalizingAngle(double rad){
+    if (rad > M_PI) {return (rad - M_PI*2);}
+    else if (rad < -M_PI) {return (rad + M_PI*2);}
+    else return rad;
 }
 
 Eigen::MatrixXd updateConfirmation(Eigen::MatrixXd prev, Eigen::MatrixXd current){
@@ -78,17 +92,26 @@ Eigen::MatrixXd updateConfirmation(Eigen::MatrixXd prev, Eigen::MatrixXd current
 }
 
 
+
 class linearKalmanFilter
 {
 private:
     bool isFirstTime;
+    bool isFlushing;
+    bool isUsingMap;
+
+    vector<double> tempX, tempY;
     double dt;    
 
+    double _x, _y, _yaw, _vx, _vy, _omega;
+    string mapLocation;
     // For ROS
     ros::Subscriber _gpsOdomSub;
     ros::Subscriber _gpsHeadingSub;
     ros::Subscriber _ERPSub;
     ros::Subscriber _imuSub;
+    
+    ros::Publisher _pubKf;
 
     double gpsX, gpsY, gpsHeading;
     double imuYawRate;
@@ -101,101 +124,274 @@ private:
 
 public:
 
+    /* 
+    <Initializing Matrix>
+    K : 6 X 6   |   F : 6 X 6
+    B : 6 X 2   |   U : 2 X 1
+    Ei: 6 X 1   |   P : 6 X 6 (diagonal) 
+    C : 6 X 4   |   Q : 6 X 6 (diagonal)
+    H : 4 X 6   |   R : 4 X 4 (diagonal)
+    Z : 4 X 1   |
+    */
     Eigen::MatrixXd prevSensorData;
-    Eigen::MatrixXd K,F,B,U,Ei,H,Z,C;
-    Eigen::MatrixXd P,Q,R;
+    Eigen::MatrixXd K,F,B,U,Ei,H,Z,C; 
+    Eigen::MatrixXd P,Q,R;            
     
-    VectorXd X,_P,_Q,_R; //diag
+    VectorXd X;
+    VectorXd _P,_Q,_R; //diag
 
-    // Eigen::MatrixXd K(6,6);
-    // Eigen::MatrixXd F(6,6);
-    // Eigen::MatrixXd B(6,2);
-    // Eigen::MatrixXd U(2,1);
-    // Eigen::MatrixXd Ei(6,1);
-    // Eigen::MatrixXd H(4,6);
-    // Eigen::MatrixXd Z(4,1);
-    // Eigen::MatrixXd C(6,4);
+    linearKalmanFilter(string mapAddress){
+        ROS_INFO("[constructor] : Constructing without Initial vehicle position");    
+        ROS_WARN("[constructor] : You have to execute PATH_SMOOTHING before construct");
+        
+        mapLocation = mapAddress;
 
+        tempX.clear(); 
+        tempY.clear();
 
-    linearKalmanFilter(double x, double y, double yaw, double vx, double vy, double omega, ros::NodeHandle *nh){
+        isUsingMap = true;
+        isFlushing = true;
+        isFirstTime = true;
 
-    isFirstTime = true;
+        gpsX = 0.0; 
+        gpsY = 0.0; 
+        gpsHeading = 0.0;
+        imuYawRate = 0.0;
 
-    erpSteer = 0;
-    erpSpeed = 0;
-    erpThrottle = 0;
+        dt = 0.01667;
+        c_r1 = 0.1;
+        c_a = 0.001;  
 
-    dt = 0.01667;
-    c_r1 = 0.1;
-    c_a = 0.001;    
+        erpSteer = 0;
+        erpSpeed = 0;
+        erpThrottle = 0;
 
-    X.setZero(6,1);
-    X << x, y, yaw, vx, vy, omega;
+    }
 
-    P = Eigen::MatrixXd::Zero(6,6);
-    Q = Eigen::MatrixXd::Zero(6,6);
-    R = Eigen::MatrixXd::Zero(4,4);
+    linearKalmanFilter(double x, double y, double yaw, double vx, double vy, double omega){
+        ROS_WARN("[constructor] : Constructing with Intial vehicle position");
+        ROS_WARN("[constructor] : Be sure to check valid data");    
+        
+        _x = x;
+        _y = y;
+        _yaw = yaw;
+        _vx = vx;
+        _vy = vy;
+        _omega = omega;
 
-    K = Eigen::MatrixXd::Zero(6,6);
-    F = Eigen::MatrixXd::Zero(6,6);
-    B = Eigen::MatrixXd::Zero(6,2);
-    U = Eigen::MatrixXd::Zero(2,1);
-    Ei = Eigen::MatrixXd::Zero(6,1);
-    H = Eigen::MatrixXd::Zero(4,6);
-    Z = Eigen::MatrixXd::Zero(4,1);
-    C = Eigen::MatrixXd::Zero(6,4);
+        isUsingMap = false;
+        isFlushing = false;
+        isFirstTime = true;
+        
+        gpsX = 0.0;
+        gpsY = 0.0;
+        gpsHeading = 0.0;
+        imuYawRate = 0.0;
 
-    _P.setZero(6);
-    _P << 0.1, 0.1, deg2rad(3.0), 0.01, 0.01, deg2rad(0.01);
-    P = _P.asDiagonal();
-    P = P.array().square();
-    cout << " Initial P" << endl << P << endl;
+        dt = 0.0;
+        c_r1 = 0.1;
+        c_a = 0.001;    
 
-    _Q.setZero(6);
-    _Q << 0.2, 0.2, deg2rad(8.0), 0.1, 0.1, deg2rad(0.7);
-    Q = _Q.asDiagonal();
-    Q = Q.array().square();
-    cout << " Initial Q" << endl << Q << endl;
+        erpSteer = 0;
+        erpSpeed = 0;
+        erpThrottle = 0;
 
-    _R.setZero(4);
-    _R << 0.08, 0.08, deg2rad(10.0), deg2rad(1.0);
-    R = _R.asDiagonal();
-    R = R.array().square();
-    cout << " Initial R" << endl << R << endl;
+    }
 
-    _gpsOdomSub = nh->subscribe("/odom", 1 , &linearKalmanFilter::gpsOdomCallback, this);
-    _gpsHeadingSub = nh->subscribe("/current_yaw", 1 , &linearKalmanFilter::gpsHeadingCallback, this);
-    _ERPSub = nh->subscribe("/erpData", 1 , &linearKalmanFilter::ERPCallback, this);
-    _imuSub = nh->subscribe("/imu", 1 , &linearKalmanFilter::imuCallback, this);
+    void rosInit(ros::NodeHandle *nh){
+        ROS_INFO("[initializer] : rosInit(), ROS Initializing..");
+        _gpsOdomSub = nh->subscribe("/odom", 1 , &linearKalmanFilter::gpsOdomCallback, this);
+        _gpsHeadingSub = nh->subscribe("/current_yaw", 1 , &linearKalmanFilter::gpsHeadingCallback, this);
+        _ERPSub = nh->subscribe("/erpData", 1 , &linearKalmanFilter::ERPCallback, this);
+        _imuSub = nh->subscribe("/imu", 1 , &linearKalmanFilter::imuCallback, this);
 
+        _pubKf = nh->advertise<geometry_msgs::Pose2D>("/LOCAL/kalmanFiltered", 10);
+        matrixInit(nh);
+    }
+
+    void matrixInit(ros::NodeHandle *nh){
+        cout << fixed;
+        cout.precision(4);
+        ROS_INFO("[initializer] : matrixInit(), Matrix Initializing..");
+        X.setZero(6,1);
+        if(isUsingMap)
+        {
+            ROS_INFO("[initializer] : matrixInit(), Waiting for GPS message.. {%s}", _gpsOdomSub.getTopic().c_str());
+            boost::shared_ptr<nav_msgs::Odometry const> sharedOdom;
+            ros::topic::waitForMessage<nav_msgs::Odometry>("/odom", *nh);
+            ROS_INFO("[initializer] : matrixInit(), Flushing GPS Data..");
+            for(int i =0; i <8; i++)
+            {
+                sharedOdom = ros::topic::waitForMessage<nav_msgs::Odometry>("/odom", *nh);
+                gpsX = sharedOdom->pose.pose.position.x;
+                gpsY = sharedOdom->pose.pose.position.y;
+                tempX.push_back(gpsX);
+                cout << "x : " << gpsX << " y : " << gpsY << endl; 
+                tempY.push_back(gpsY);
+                sleep(0.125);
+            }
+
+            ROS_INFO("[initializer] : matrixInit(), Calculating Initial GPS Data..");
+            vector<double> xyS = getInitialGPS(tempX, tempY);
+            ROS_INFO("[initializer] : matrixInit(), Calculating Initial Vehicle Yaw..");
+            double initialYaw = getInitialYaw(mapLocation);
+        
+            X << xyS[0], xyS[1], initialYaw, 0.01, 0.0, 0.0;
+
+            isFlushing = false;
+        }
+        else
+        {
+            X << _x, _y, _yaw, _vx, _vy, _omega;
+        }
+        
+        P = Eigen::MatrixXd::Zero(6,6);
+        Q = Eigen::MatrixXd::Zero(6,6);
+        R = Eigen::MatrixXd::Zero(4,4);
+
+        K = Eigen::MatrixXd::Zero(6,6);
+        F = Eigen::MatrixXd::Zero(6,6);
+        B = Eigen::MatrixXd::Zero(6,2);
+        U = Eigen::MatrixXd::Zero(2,1);
+        Ei = Eigen::MatrixXd::Zero(6,1);
+        H = Eigen::MatrixXd::Zero(4,6);
+        Z = Eigen::MatrixXd::Zero(4,1);
+        C = Eigen::MatrixXd::Zero(6,4);
+
+        cout << "Initial X -> " << endl << X << endl;
+
+        _P.setZero(6);
+        _P << 0.1, 0.1, deg2rad(3.0), 0.01, 0.01, deg2rad(0.01);
+        P = _P.asDiagonal();
+        P = P.array().square();
+        cout << " Initial P -> " << endl << P << endl;
+
+        _Q.setZero(6);
+        _Q << 0.1, 0.1, deg2rad(2.0), 0.01, 0.01, deg2rad(0.01);
+        Q = _Q.asDiagonal();
+        Q = Q.array().square();
+        cout << " Initial Q -> " << endl << Q << endl;
+
+        _R.setZero(4);
+        _R << 0.2, 0.2, deg2rad(5.0), deg2rad(1.0);
+        R = _R.asDiagonal();
+        R = R.array().square();
+        cout << " Initial R -> " << endl << R << endl;
+
+    }
+
+    vector<double> getInitialGPS(vector<double> x, vector<double> y){
+        ROS_WARN("getInitialGPS(), x.size() : %ld, y.size() : %ld",x.size(),y.size());
+        if(x.size()!= y.size())
+        {
+            ROS_ERROR("getInitialGPS(), the length of the arguments is different");
+            exit(0);
+        }
+        double meanX = std::accumulate(x.begin(), x.end(), 0.0) / x.size();
+        double meanY = std::accumulate(y.begin(), y.end(), 0.0) / y.size();
+
+        vector<double> xy;
+        xy.push_back(meanX); xy.push_back(meanY);
+        ROS_INFO("getInitialGPS(), Set Initial Position as X : %f, Y: %f",meanX, meanY);
+
+        return xy;
+    }
+
+    vector<string> split(string sentence, char Separator){
+        vector<string> answer;
+        stringstream ss(sentence);
+        string tmp;
+    
+        while (getline(ss, tmp, Separator)) {
+            answer.push_back(tmp);
+        }
+        return answer;
+    }
+
+    double getInitialYaw(string mapAdd){
+        string line;
+        ifstream map(mapAdd);
+        double initialYaw = 0.0;
+
+        vector<double> mapX, mapY;
+        mapX.clear(); mapY.clear();
+
+        for(int i = 0; i<5; i++)
+        {
+            if (map.is_open())
+            {
+                getline(map, line);
+
+                vector<string> fromMap = split(line,',');
+                ROS_INFO("getInitialYaw(), Getting line %d x :%s, y:%s",i,fromMap[0].c_str(), fromMap[1].c_str());
+                mapX.push_back(stod(fromMap[0])); mapY.push_back(stod(fromMap[1]));
+            }
+            else
+            {
+                ROS_ERROR("getInitialYaw(), Having trouble loading the Global Map.");
+                exit(0);
+            }
+        }
+
+        initialYaw = atan2(mapY.back()-mapY.front(), mapX.back()-mapX.front());
+        ROS_INFO("getInitialYaw(), Initial Vechicle Yaw is : %f", initialYaw * 180/M_PI);
+        map.close();
+
+        return initialYaw;
+        
     }
 
     void gpsOdomCallback(const nav_msgs::Odometry::ConstPtr& Odom){
-        gpsX = Odom->pose.pose.position.x;
-        gpsY = Odom->pose.pose.position.y;
-        // ROS_INFO("GPS Odom callback works");
+        if(isFlushing)
+        {
+            PASS;
+        }
+        else
+        {
+            gpsX = Odom->pose.pose.position.x;
+            gpsY = Odom->pose.pose.position.y;
+        }           
     }
 
     void gpsHeadingCallback(const std_msgs::Float64::ConstPtr& Heading){
-        gpsHeading = Heading->data;
-        // ROS_INFO("GPS Heading callback works");
+        if(isFlushing)
+        {
+            PASS;
+        }
+        else
+        {
+            gpsHeading = Heading->data;
+        }
     }
 
     void ERPCallback(const control_to_serial::fromERP::ConstPtr& ERP){
-        erpSteer = ERP->steer / 71 * M_PI / 180;
-        erpThrottle = (erpSpeed - ERP->speed / 36) / dt;
-        erpSpeed = ERP->speed / 36;
-        // ROS_INFO("ERP callback works");
+        if(isFlushing)
+        {
+            PASS;
+        }
+        else
+        {
+            erpSteer = ERP->steer / 71 * M_PI / 180;
+            erpThrottle = (erpSpeed - ERP->speed / 36) / dt;
+            erpSpeed = ERP->speed / 36;
+        }
     }
 
     void imuCallback(const sensor_msgs::Imu::ConstPtr& Imu){
-        imuYawRate = Imu->angular_velocity.z;
-        calcSate();
-        // ROS_INFO("IMU callback works");
+        if(isFlushing)
+        {
+            PASS;
+        }
+        else
+        {
+            imuYawRate = Imu->angular_velocity.z;
+            calcSate();
+        }
     }
 
     void calcSate(){
         double begin = ros::Time::now().toSec();
+
         F << 1, 0, 0, cos((X(2))) * dt, -sin(double(X(2))) * dt, 0,
             0, 1, 0, sin(double(X(2))) * dt, cos(double(X(2))) * dt, 0,
             0, 0, 1, 0, 0, dt,
@@ -213,33 +409,23 @@ public:
         U << erpThrottle,
              erpSteer;
         
-
         double Ffy = -CF * atan2(((X(4) + LF * X(5)) / X(3) - erpSteer), 1.0);
         double Fry = -CR * atan2(((X(4) - LR * X(5)) / X(3)), 1.0);
         double R_x = c_r1 * double(X(3));
         double F_aero = c_a * X(3) * X(3);
         double F_load = F_aero + R_x;
-        // cout << "Ffy    : " << Ffy << endl;
-        // cout << "Fry    : " << Fry << endl;
-        // cout << "R_x    : " << R_x << endl;
-        // cout << "F_aero : " << F_aero << endl;
-        // cout << "F_load : " << F_load << endl;
 
         double Ei_3 = (-(Ffy * sin(erpSteer)) * dt / M) - (F_load * dt) /M;
         double Ei_4 = (Fry * dt) / M + (Ffy * cos(erpSteer)) / M;
         double Ei_5 = ((Ffy * LF * cos(erpSteer) - Fry * LR) * dt / IZ);
+
         Ei << 0,
               0,
               0,
               Ei_3,
               Ei_4,
               Ei_5;
-        // Ei << 0,
-        //       0,
-        //       0,
-        //       (-(Ffy * sin(erpSteer)) * dt / M - F_load * dt /M),
-        //       (Fry * dt / M + Ffy * cos(erpSteer) / M),
-        //       ((Ffy * LF * cos(erpSteer) - Fry * LR) * dt / IZ);
+
 
         H << 1, 0, 0, 0, 0, 0,
              0, 1, 0, 0, 0, 0,
@@ -251,14 +437,6 @@ public:
              gpsHeading, 
              imuYawRate;
         
-        // Not yet
-        // C << 1, 0, 0, 0,
-        //      0, 1, 0, 0,
-        //      0, 0, 1, 0,
-        //      0, 0, 0, 0,
-        //      0, 0, 0, 0,
-        //      0, 0, 0, 1;
-
         C = updateConfirmation(prevSensorData, Z);
 
         if(isFirstTime)
@@ -279,7 +457,6 @@ public:
         S2 = C*S1*C.transpose();
         pinv = S2.completeOrthogonalDecomposition().pseudoInverse();
 
-        // pinv = S2.completeOrthogonalDecomposition().pseudoInverse();
         K = P*H.transpose()*C.transpose()*pinv;
 
         X = X + K*C*(Z - H*X);
@@ -288,9 +465,18 @@ public:
 
 
         prevSensorData = Z;
+
         double finish = ros::Time::now().toSec();
+        dt = finish - begin;
+        
+        geometry_msgs::Pose2D pose;
+        pose.x = X(0);
+        pose.y = X(1);
+        pose.theta = X(2);
+        _pubKf.publish(pose);
         // ROS_INFO("Calculated Yaw : %f", double(X(2,0))* 180.0 /M_PI);
-        ROS_INFO("Hz : %f", 1/(finish-begin));
+        // ROS_INFO("time delta : %f", dt);
+        ROS_INFO("[State] : calcState(), X : %f , Y : %f, Yaw : %f", X(0), X(1), normalizingAngle(X(2))* 180.0 /M_PI);
     }
 
     vehicleState getStates()
@@ -310,17 +496,25 @@ public:
 int main(int argc, char **argv){
 
     ros::init(argc, argv, "cpp_test");
-    ros::NodeHandle nh;
-    cout << "whats wrong??..." << endl;
-    MatrixXd mat(3,3);
-    // mat = MatrixXd::
-    cout << mat << endl;
-    double m = 1;
-    cout << m * cos(m) << endl;
-    cout << "x : " << deg2rad(30) << endl;
-    linearKalmanFilter lkf = linearKalmanFilter(346297.8422, 4069642.2869,-45.0 * M_PI/180.0, 0.01, 0.0, 0.0, &nh);               
-    
-    ros::spin();
+    ros::NodeHandle nh("~");
+    string mapAdd;
 
+    if(nh.getParam("map",mapAdd))
+    {
+        ROS_INFO("[Constructor] : Using Global Map to set initial position");
+        linearKalmanFilter lkf = linearKalmanFilter(mapAdd);
+        lkf.rosInit(&nh);
+
+        ros::spin();
+    }
+    else
+    {
+        ROS_INFO("[Constructor] : Not Using Global Map");
+        ROS_WARN("[Constructor] : Please Check the validation of your inputs ");
+        linearKalmanFilter lkf = linearKalmanFilter(346297.3469, 4069642.8422,-45.0 * M_PI/180.0, 0.01, 0.0, 0.0);
+        
+        ros::spin();
+    }
+    
     return 0;
 }
